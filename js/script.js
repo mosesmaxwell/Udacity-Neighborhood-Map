8548ea730a0d654859330d6f8ff747b6281e7347
DESKTOP-MACDSH7\maxwell <mosesmaxwell@gmail.com>
2018-01-21 11:13:15 +0530

    return marker icon image fixed

--- js/script.js
+++ js/script.js
@@ -165,8 +165,18 @@ function initMap() {
     ko.applyBindings(venues);
 }
 
+// This function takes in a COLOR, and then creates a new marker
+// icon of that color. The icon will be 21 px wide by 34 high, have an origin
+// of 0, 0 and be anchored at 10, 34).
 function makeMarkerIcon(markerColor) {
-    return icon = 'http://www.googlemapsmarkers.com/v1/009900/';
+    var markerImage = new google.maps.MarkerImage(
+        'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor +
+        '|40|_|%E2%80%A2',
+        new google.maps.Size(21, 34),
+        new google.maps.Point(0, 0),
+        new google.maps.Point(10, 34),
+        new google.maps.Size(21, 34));
+    return markerImage;
 }
 
 function mapError() {
